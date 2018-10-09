a01b5a5ff86204361461c7511673ad86c78dee17
sdiperi90 <sirgasheva17@gmail.com>
2018-10-09 16:45:47 -0400

    set up forms and added post route to open weather API

---
+++ public/index.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+    var nextBtn = $("#next-btn");
+    nextBtn.click(event => {});
+});
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
 var path = require("path");
+var axios = require("axios");
 
 app.use(express.static(path.join(__dirname, "public")));
 // whatever data comes from json parse it and put it on body object example: req.body
@@ -17,4 +18,21 @@ var server = app.listen(3000, () => {
 
 app.get("/", (req, res) => {
     res.render("index");
+});
+
+app.get("/weather-forecast", (req, res) => {
+    res.render("new-page");
+});
+
+app.post("/weather-forecast", (req, res) => {
+    var zipcode = req.body.zipcode;
+    console.log(req.body.zipcode);
+    axios
+        .get(
+            `http://api.openweathermap.org/data/2.5/weather?zip=${zipcode},us&APPID=5ff3c2d31df65ddb5baf09addb40c4d7`
+        )
+        .then(function(data) {
+            console.log(data[0]);
+            res.render("new-page");
+        });
 });
